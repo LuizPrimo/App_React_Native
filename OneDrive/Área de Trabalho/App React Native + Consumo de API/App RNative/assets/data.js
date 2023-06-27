d1e5d03310c83ffacbee28d0818a7e52eacd0e2c
luizprimo.1a2015@gmail.com <luizprimo.1a2015@gmail.com>
2023-06-27 19:58:46 -0300

    App RNative

---
+++ OneDrive/Área de Trabalho/App React Native + Consumo de API/App RNative/assets/data.js
@@ -0,0 +1,15 @@
+export const dummyData =
+        [{
+                url: 'https://i.ibb.co/WpDHdZ9/1.jpg',
+                id: 1
+
+        },
+        {
+                url: 'https://i.ibb.co/wzpfhFS/2.jpg',
+                id: 2
+        },
+        {
+                url: 'https://i.ibb.co/ky2dttV/3.jpg',
+                id: 3
+        }
+        ]
